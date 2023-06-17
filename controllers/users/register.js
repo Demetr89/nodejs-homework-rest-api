ba05ccb7325b6c283d67ca5f3bbd5ed327b5a385
Dikusar Dmyuro <dima.dikusar.89@gmail.com>
2023-06-17 15:00:06 +0300

    hw5 is done

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,24 +1,26 @@
-const { User } = require('../../models/user');
-const { RequestError } = require('../../helpers');
-const bcrypt = require('bcryptjs');
+const { User } = require("../../models/user");
+const { RequestError } = require("../../helpers");
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-        throw RequestError(409, 'Email in use')
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, "Email in use");
+  }
 
-    const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-    const result = await User.create({ email, password: hashPass });
-    res.status(201).json({
-        status: 'created',
-        code: 201,
-        data: {
-            user: result,
-        },
-    });
+  const avatarURL = gravatar.url(email);
+  const result = await User.create({ email, password: hashPass, avatarURL });
+  res.status(201).json({
+    status: "created",
+    code: 201,
+    data: {
+      user: result,
+    },
+  });
 };
 
 module.exports = register;
